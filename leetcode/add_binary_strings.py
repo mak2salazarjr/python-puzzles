3fd6df235a79077709e91954e2e6f2f5148c8770
Tom Semple <semple.tom@gmail.com>
2017-04-01 18:33:40 -0700

    Add README.md and vcs.xml Add area_of_a_convex_polygon.py Add first tests for checkio and leetcode

---
+++ leetcode/add_binary_strings.py
@@ -0,0 +1,29 @@
+from collections import namedtuple
+
+
+class Solution(object):
+    def addBinary(self, *bin_strs):
+        """
+        Add binary strings
+        :param bin_strs: zero or more strings representing a binary number
+        :return: a string representing sum
+        """
+        return '{:b}'.format(sum([int(bin_str, base=2) for bin_str in bin_strs]))
+
+
+if __name__ == '__main__':
+    Test = namedtuple('Test', 'data expect')
+    add_binary_strings = Solution().addBinary
+
+    tests = (Test(tuple(), '0'),
+             Test(('0',), '0'),
+             Test(('1010', '11'), '1101'),
+             Test(('1' * 100, '1'), '1' + '0' * 100),
+             Test(('10' * 100, '11'), '10' * 98 + '1101'),
+             Test(('1111', '111', '101'), '11011'))
+
+    for test in tests:
+        result = add_binary_strings(*test.data)
+        print(f'{test.data} -> {result}')
+        if test.expect:
+            assert result == test.expect
